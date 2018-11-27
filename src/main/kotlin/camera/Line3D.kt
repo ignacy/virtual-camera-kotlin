6bdca2b0aee491e492c0aa462b04f6b799f2ab74
Ignacy Moryc <imoryc@protonmail.com>
2018-11-27 21:20:19 +0100

    Draw with broad stroke

--- src/main/kotlin/camera/Line3D.kt
+++ src/main/kotlin/camera/Line3D.kt
@@ -4,16 +4,23 @@ import algebra.Matrix
 import java.awt.Color
 import java.awt.Graphics
 import java.util.ArrayList
+import java.awt.BasicStroke
+import java.awt.Graphics2D
+
+
 
 class Line3D(private val start: Point3D, private val end: Point3D) {
     fun draw(graphics: Graphics, camera: Camera, scene: Scene, color: Color) {
         val transformedStart = start.transform(camera, scene)
         val transformedEnd = end.transform(camera, scene)
 
-        graphics.color = color
+        val g2 = graphics as Graphics2D
+        g2.stroke = BasicStroke(3f)
+
+        g2.color = color
 
         if (camera.isVisible(end) && camera.isVisible(start)) {
-            graphics.drawLine(
+            g2.drawLine(
                     transformedStart.x.toInt(),
                     transformedStart.y.toInt(),
                     transformedEnd.x.toInt(),
