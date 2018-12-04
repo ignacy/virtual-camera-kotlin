1175c7107e3c2f85347853e35d11839280d1b993
Ignacy Moryc <imoryc@protonmail.com>
2018-12-04 21:02:59 +0100

    Remove some more unused code

--- src/main/kotlin/gui/DrawingContext.kt
+++ src/main/kotlin/gui/DrawingContext.kt
@@ -2,9 +2,17 @@ package gui
 
 import camera.Camera
 import camera.Scene
-import java.awt.BasicStroke
-import java.awt.Graphics
-import java.awt.Graphics2D
+import javax.swing.*
+import java.awt.*
+
+class Canvas (var scene: Scene, private val camera: Camera) : JPanel() {
+    override fun paintComponent(graphics: Graphics) {
+        super.paintComponent(graphics)
+        super.setBackground(Color.decode("#dae1e7"))
+        graphics.drawRect(0, 0, Scene.HEIGHT, Scene.WIDTH)
+        DrawingContext(graphics, scene, camera).draw()
+    }
+}
 
 class DrawingContext {
     val graphics : Graphics
